ba6f477a2b4ae4a623dd1dd6e322f2c5115ff507
Patrick Loeve <patrickloeve@hey.com>
2025-02-05 13:18:43 +0100

    Change database_url with correct env variables

--- backend/db.py
+++ backend/db.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/{os.getenv('POSTGRES_DB')}"
+DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:5432/{os.getenv('POSTGRES_DB')}"
 
 # SQLAlchemy database setup
 engine = create_engine(DATABASE_URL)
